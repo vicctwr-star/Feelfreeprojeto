5f5a131fed8f07511992c4ce96f2fd1eabf9a5f2
Kamilly Vitoria <vicctwr@gmail.com>
2025-10-02 19:36:03 +0000

    script

---
+++ src/feelfree/assets/script/login.js
@@ -0,0 +1,20 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nome = document.querySelector("#nome");
+  const senha = document.querySelector("#senha");
+  login(nome.value, senha.value);
+});
+
+async function login(nome, senha) {
+  console.log({ nome, senha });
+  const resposta = await fetch("https://kxtq5f-3000.csb.app/login", {
+    method: "POST",
+    body: JSON.stringify({ nome, senha }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+
+  const json = await resposta.json();
+  console.log(json);
+}
