768084ba3356a11968d6e77daa756ec305a78fd5
Kamilly Vitoria <vicctwr@gmail.com>
2025-10-29 20:26:22 +0000

    alteraçoes

---
+++ assets/script/comentarios.js
@@ -0,0 +1,84 @@
+const postagem = {
+  id: 1,
+  autor: "kenny",
+  imagem: "../assets/images/transferir 2 (2).svg",
+  conteudo: "autistas KKKKK",
+  qntLikes: 0,
+  data: 12,
+  comentarios: [
+    {
+      id: 44,
+      autor: "vi",
+      imagem: "../assets/images/transferir 2 (2).svg",
+      conteudo: "autistas KKKKlllK",
+      qntLikes: 0,
+      data: 12,
+    },
+    {
+      id: 44,
+      autor: "vi",
+      imagem: "../assets/images/transferir 2 (2).svg",
+      conteudo: "autistas KKKKlllK",
+      qntLikes: 0,
+      data: 12,
+    },
+  ],
+};
+document.querySelectorAll(".publi")[0].innerHTML = `
+<div class="post">
+          <div class="post-header">
+            <img
+              src="${postagem.imagem}"
+              alt="Perfil"
+              class="post-perfil"
+            />
+            <div class="post-user">
+              <strong>${postagem.autor}</strong>
+              <span>há 1 min</span>
+            </div>
+            <div class="opciones_div">
+              <img
+                src="../assets/images/ellipsis-vertical-solid-full 1.svg"
+                alt="mas opciones"
+                class="opciones"
+              />
+            </div>
+          </div>
+          <p>${postagem.conteudo}</p>
+          <div class="post-actions">
+            <img src="../assets/images/Frame (2).svg" alt="like" />
+            <img src="../assets/images/Frame (1).svg" alt="comentario" />
+          </div>
+        </div>
+`;
+
+document.querySelectorAll(".publi")[1].innerHTML = "";
+postagem.comentarios.forEach((comentario) => {
+  document.querySelectorAll(".publi")[1].innerHTML += `
+<div class="post1">
+          <div class="post-header">
+            <img
+              src="${comentario.imagem}"
+              alt="Perfil"
+              class="post-perfil"
+            />
+            <div class="post-user">
+              <strong>${comentario.autor}</strong>
+              <span>há 1 min</span>
+            </div>
+            <div class="opciones_div">
+              <img
+                src="../assets/images/ellipsis-vertical-solid-full 1.svg"
+                alt="mas opciones"
+                class="opciones"
+              />
+            </div>
+          </div>
+          <p>${comentario.conteudo}</p>
+          <div class="post-actions">
+            <img src="../assets/images/Frame (2).svg" alt="like" />
+            <img src="../assets/images/Frame (1).svg" alt="comentario" />
+          </div>
+        </div>
+`;
+});
---
+++ assets/script/comunidade.js
@@ -0,0 +1,80 @@
+const postagens = [
+  {
+    id: 1,
+    autor: "kenny",
+    imagem: "../assets/images/transferir 2 (2).svg",
+    conteudo: "autistas KKKKK",
+    qntLikes: 0,
+    data: 12,
+  },
+  {
+    id: 2,
+    autor: "kily",
+    imagem: "../assets/images/transferir 2 (2).svg",
+    conteudo: "mentira KKKKK",
+    qntLikes: 0,
+    data: 12,
+  },
+];
+
+document.querySelector(".publi").innerHTML = "";
+
+function postar(postagem) {
+  document.querySelector(".publi").innerHTML += `<div class="post">
+    <div class="post-header">
+      <img
+        src="${postagem.imagem}"
+        alt="Perfil"
+        class="post-perfil"
+      />
+      <div class="post-user">
+        <strong>${postagem.autor}</strong>
+        <span>há 1 min</span>
+      </div>
+      <div class="opciones_div">
+        <img
+          src="../assets/images/ellipsis-vertical-solid-full 1.svg"
+          alt="mas opciones"
+          class="opciones"
+        />
+      </div>
+    </div>
+    <p>${postagem.conteudo}</p>
+    <div class="post-actions">
+      <img class="like-${postagem.id} vazio" onclick="darLike(${postagem.id})" src="../assets/images/Frame (2).svg" alt="like" />
+      <a href="/pages/comentarios.html">
+        <img src="../assets/images/Frame (1).svg"alt="comentario"/>
+      </a>
+      </div>
+    </div>`;
+}
+
+postagens.forEach((postagem) => {
+  postar(postagem);
+});
+
+function darLike(postagemId) {
+  const coracao = document.querySelector(".like-" + postagemId);
+  if (coracao.classList.contains("vazio")) {
+    coracao.src = "../assets/images/vermelçho.svg";
+  } else {
+    coracao.src = "../assets/images/Frame (2).svg";
+  }
+
+  coracao.classList.toggle("vazio");
+}
+
+document.querySelector("form").addEventListener("submit", (even) => {
+  even.preventDefault();
+  const postagemNova = {
+    autor: "bla",
+    conteudo: document.querySelector("textarea").value,
+    imagem: "../assets/images/transferir 2 (2).svg",
+    qntLikes: 0,
+    data: 12,
+    id: 3,
+  };
+
+  postagens.push(postagemNova);
+  postar(postagemNova);
+});
